1d0aef8b7d75d014598249381c7c40f803ae9cab
Dung le <dle8@gpu-node01.csug.rochester.edu>
2020-07-26 17:34:31 -0400

    C++ array init ways

---
+++ containers/array.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // init arrays
+    int foo [5];
+    int foo1 [5] = { 16, 2, 77, 40, 12071 }; 
+    int bar [5] = { 10, 20, 30 }; // {10, 20, 30, 0, 0};
+    int foo2 [] = { 16, 2, 77, 40, 12071 }; // size = 5
+
+    int foo3[] { 10, 20, 30 };  // universal init -> no equal sign
+    return 0;
+}
