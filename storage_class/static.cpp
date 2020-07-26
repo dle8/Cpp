1ad9de5db3c55395a3216334c5f7014be9addafe
Dung le <dle8@cycle1.csug.rochester.edu>
2020-07-26 15:54:58 -0400

    Add static

--- storage_class/static.cpp
+++ storage_class/static.cpp
@@ -1,7 +1,9 @@
 /*
-    static function indicates that it is not associated with a particular object.
-    static local variable allows the function to perserve information between calls without introducing a global variable that might be accessed and corrupted by other functions (*)
-    static member: a variable that is part of a class, yet is not part of an object of that class. There is exactly one copy of a static member instead of one object of that class (like non-static members)
+    static function: indicates that it is not associated with a particular object.
+    static local variable: allows the function to perserve information between calls without introducing a global variable that might be 
+        accessed and corrupted by other functions (*)
+    static member: a variable that is part of a class, yet is not part of an object of that class. There is exactly one copy of a static 
+        member instead of one object of that class (like non-static members)
 */
 
 #include <bits/stdc++.h>
