fd28bbbed52ddb1367715f8fc2a50de1636ac36c
Dylan Le <dle8@u.rochester.edu>
2020-09-20 19:37:11 -0700

    Update notes for inheritance and polymorphism

--- oop/inheritance.cpp
+++ oop/inheritance.cpp
@@ -1,12 +1,17 @@
 /*
-    Multiple Inheritance is a feature of C++ where a class can inherit from more than one classes. The constructors of inherited classes are called in the same order in which they are inherited.
+    Multiple Inheritance is a feature of C++ where a class can inherit from more than one classes.
+    The constructors of inherited classes are called in the same order in which they are inherited.
 
-    In here we have diamond problem, which occurs when two superclasses of a class have a common base class. Use base class as virtual base. The language ensures that a constructor of a virtual base is called exactly one.
-    the constructor of every virtual base is invoked (implicitly or explicitly) from the constructor of the most derived class to make sure constructor of base is called before its derived classes. If there are multiple constructor (parameterized ones and default),
-    parameterized constructors can only be called within most derived class.
+    In here we have diamond problem, which occurs when two superclasses of a class have a common base class.
+    Use base class as virtual base. The language ensures that a constructor of a virtual base is called exactly one.
+    the constructor of every virtual base is invoked (implicitly or explicitly) from the constructor of the most
+    derived class to make sure constructor of base is called before its derived classes. If there are multiple constructor
+    (parameterized ones and default), parameterized constructors can only be called within most derived class (****).
 
-    Overriding virtual base functions: If different derived classes override the same function? This is only allow iff one function of a derived class override those of the others. If two classes override a base class function,
-    but neither override the other, the class hierachy is an error (ambiguous). That's why class TA need function show (***) else the compiler will report error.
+    Overriding virtual base functions: If different derived classes override the same function? This is only allow iff
+    one function of a derived class override those of the others. If two classes override a base class function,
+    but neither override the other, the class hierarchy is an error (ambiguous). That's why class TA need function show
+    (***) else the compiler will report error.
 */
 
 #include<iostream> 
@@ -55,7 +60,7 @@ public:
 
     using Student::show;
 
-    TA(int x):Student(x), Faculty(x), Person(x)  { 
+    TA(int x):Student(x), Faculty(x), Person(x)  { // ****
         cout<<"TA::TA(int ) called"<< endl; 
     } 
 
--- oop/polymorphism.cpp
+++ oop/polymorphism.cpp
@@ -1,15 +1,19 @@
 /*
-Polymorphism means having many form. Typically, polymorphism occurs when there's a hierachy of classes and they are related by inheritance.
-C++ polymorphiims means that a call to a member function will cause a different functino ot be executed depending on the type of object that
+Polymorphism means having many form. Typically, polymorphism occurs when there's a hierachy of classes and they are
+ elated by inheritance.
+C++ polymorphiims means that a call to a member function will cause a different functino ot be executed depending on the
+ type of object that
 invokes the function.
 
-Without virtual keyword, the function area will be set once by the compiler as the version defined in the base class. This is called static
-resolution of the function, or static linkage, or early binding (area() function is set during the compilation of the program).
+Without virtual keyword, the function area will be set once by the compiler as the version defined in the base class.
+ This is called static
+resolution of the function, or static linkage, or early binding (area() function is set during the compilation of the
+ program).
 
-Virtual function is a function in a base class that is declared using the keyword virtual. Defining in a base class a virtual function, with
-another version in a derived class, signals to the compiler that static linkage is not wanted for this function. What is wanted is the selection
-of the funciton to be called at any given point in the program to be based on the kind of object for which it's called - dynamic linkage, or late
-binding.
+Virtual function is a function in a base class that is declared using the keyword virtual. Defining in a base class a
+ virtual function, with another version in a derived class, signals to the compiler that static linkage is not wanted
+ for this function. What is wanted is the selection of the funciton to be called at any given point in the program to be
+ based on the kind of object for which it's called - dynamic linkage, or late binding.
 
 */
 
